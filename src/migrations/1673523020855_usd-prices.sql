970d1fadd8b0a1aef4016e25ae310478ca98683a
$hubh@m <shubham@priori.capital>
2023-01-12 17:02:02 +0530

    Added migration.

--- src/migrations/1673523020855_usd-prices.sql
+++ src/migrations/1673523020855_usd-prices.sql
@@ -4,12 +4,12 @@ CREATE TABLE "usd_prices" (
   "timestamp" TIMESTAMPTZ NOT NULL,
   "value" NUMERIC NOT NULL,
   "chain_id" NUMERIC NOT NULL,
-  "coingecko_id" TEXT NOT NULL,
+  "coingecko_id" TEXT NOT NULL
 );
 
 ALTER TABLE "usd_prices"
   ADD CONSTRAINT "usd_prices_pk"
-  PRIMARY KEY ("currency", "chainId", "timestamp");
+  PRIMARY KEY ("currency", "chain_id", "timestamp");
 
 -- Down Migration
 DROP TABLE "usd_prices";
