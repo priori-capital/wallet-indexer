7a5382528f88bda35db717e919b387e5460d7c11
$hubh@m <shubham@priori.capital>
2023-01-19 14:12:06 +0530

    Fix usd prices.

--- src/scripts/update-usd-prices.ts
+++ src/scripts/update-usd-prices.ts
@@ -1,11 +1,12 @@
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
+
 import axios from "axios";
 import moment from "moment";
 
 import { idb } from "@/common/db";
-import { config as dotEnvConfig } from "dotenv";
-dotEnvConfig();
 
 const fetchAssetMarketPriceInUSD = async () => {
   const assetsWithUSDPricesURL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10`;
