5c9eb82dbbbc9e8e462fca8aec884704a0d8df3d
Gulfam Ali <gulfam.work@gmail.com>
2023-04-07 15:01:01 +0530

    chore: adding debug logs

--- src/jobs/activities/wallet-activity-tracking.ts
+++ src/jobs/activities/wallet-activity-tracking.ts
@@ -4,6 +4,7 @@ import { isCachedWallet } from "@/utils/in-memory-cache";
 import { Queue, QueueOptions } from "bullmq";
 
 import { TransferEventData } from "./transfer-activity";
+import { logger } from "@/common/logger";
 
 const WALLET_TRANSACTION_LOGS_QUEUE_NAME = "wallet-transaction-logs-queue";
 const WALLET_TRANSACTION_LOGS_JOB_NAME = "wallet-transaction-logs";
@@ -33,25 +34,32 @@ export const walletTransactionLogsQueue = new Queue(
 
 export class WalletActivityTracking {
   static async handleEvent(transferEvent: TransferEventData): Promise<void> {
-    const isFromAddressTracked = await isCachedWallet(transferEvent.fromAddress);
-    let isToAddressTracked = false;
-    if (!isFromAddressTracked) {
-      isToAddressTracked = await isCachedWallet(transferEvent.toAddress);
-    }
+    try {
+      const isFromAddressTracked = await isCachedWallet(transferEvent.fromAddress);
+      let isToAddressTracked = false;
+      if (!isFromAddressTracked) {
+        isToAddressTracked = await isCachedWallet(transferEvent.toAddress);
+      }
 
-    const isTrackedAddressTransaction = isFromAddressTracked || isToAddressTracked;
+      const isTrackedAddressTransaction = isFromAddressTracked || isToAddressTracked;
 
-    if (isTrackedAddressTransaction) {
-      const transaction = await getTransaction(
-        transferEvent.chainId,
-        transferEvent.transactionHash
-      );
-      const payload: WalletActivityEvent = {
-        transaction,
-        transferEvent,
-      };
-
-      await walletTransactionLogsQueue.add(WALLET_TRANSACTION_LOGS_JOB_NAME, payload);
+      if (isTrackedAddressTransaction) {
+        const transaction = await getTransaction(
+          transferEvent.chainId,
+          transferEvent.transactionHash
+        );
+        const payload: WalletActivityEvent = {
+          transaction,
+          transferEvent,
+        };
+        logger.info(`WalletActivityTracker`, `Tracking: TxHash: ${transaction?.hash}`)
+        logger.info(`WalletActivityTracker`, `Adding Job Data: chainId: ${transferEvent?.chainId},  TxHash: ${transferEvent?.transactionHash}`)
+        await walletTransactionLogsQueue.add(WALLET_TRANSACTION_LOGS_JOB_NAME, payload);
+      }
+    } catch(err) {
+      logger.info(`WalletActivityTracker`, `chainId: ${transferEvent?.chainId},  TxHash: ${transferEvent?.transactionHash}`)
+      logger.error(`WalletActivityTracker`, `${err}`);
+      throw err;
     }
   }
 }
