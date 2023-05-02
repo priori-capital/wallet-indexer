862ad91c8fc930ce08e0802f3719fe7fd017aa08
Chinmay Bag <chinmay2893@gmail.com>
2023-05-02 11:22:18 +0530

    fix name for primary key constraints

--- src/migrations/1683006365108_alter-tracked-wallets-pk.sql
+++ src/migrations/1683006365108_alter-tracked-wallets-pk.sql
@@ -1,9 +1,9 @@
 -- Up Migration
 
 ALTER TABLE "tracked_wallets" DROP CONSTRAINT tracked_wallets_pk;
-ALTER TABLE "tracked_wallets" ADD PRIMARY KEY ("address", "account_id");
+ALTER TABLE "tracked_wallets" ADD CONSTRAINT tracked_wallets_pk PRIMARY KEY ("address", "account_id");
 
 -- Down Migration
 
 ALTER TABLE "tracked_wallets" DROP CONSTRAINT tracked_wallets_pk;
-ALTER TABLE "tracked_wallets" ADD PRIMARY KEY ("address");
+ALTER TABLE "tracked_wallets" ADD CONSTRAINT tracked_wallets_pk PRIMARY KEY ("address");
